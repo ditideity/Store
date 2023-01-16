bfc1134121af331cffcf83065bb143ebaf4fdbf6
ditideity <106046081+ditideity@users.noreply.github.com>
2023-01-16 10:08:20 +0530

    Create dict.py

---
+++ dict.py
@@ -0,0 +1,15 @@
+#Defining Dictionary
+dictionary = {
+    'a':float(input("Enter float (assigned to a): ")),
+    'b':float(input("Enter another float (assigned to b): ")),
+    'c':float(input("Enter another float (assigned to c): ")),
+    'd':float(input("Enter another float (assigned to d): ")),
+    'e':float(input("Enter another float (assigned to e): ")),
+}
+
+#Printing to show data-type is dictionary
+x=type(dictionary)
+print("Data type of dictionary is:",x)
+
+#Printing all elements and keys
+print("Dictionary is:",dictionary)
